cb2e3caa3c93377bfb46c8debd807b46d1cd6b7d
IronLinkSubrogation <ironlinksubrogation@gmail.com>
2025-07-21 20:36:19 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
+
+const App = () => (
+  <div style={{ fontFamily: 'Arial, sans-serif' }}>
+    <Dashboard />
+  </div>
+);
+
+export default App;
