eeeea035ba55ebae24366cb8501cb63b4af68729
IronLinkSubrogation <ironlinksubrogation@gmail.com>
2025-07-24 20:33:41 -0500

    Update index.js

--- src/routes/case/[id]/index.js
+++ src/routes/case/[id]/index.js
@@ -1,9 +1,5 @@
 import CaseDetailDrawer from './CaseDetailDrawer';
 
 export default function CaseRoute() {
-  return (
-    <div className="relative w-full h-screen">
-      <CaseDetailDrawer />
-    </div>
-  );
+  return <CaseDetailDrawer />;
 }
