6293aa953202122b9500b994a7cf8de030bb575f
IronLinkSubrogation <ironlinksubrogation@gmail.com>
2025-07-24 21:18:28 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  server: {
+    port: 5173,
+    open: true
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
   }
 })
