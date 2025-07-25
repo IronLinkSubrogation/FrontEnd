a428558dba851b532591f401b51972d22cb635fe
IronLinkSubrogation <ironlinksubrogation@gmail.com>
2025-07-24 20:59:23 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import CasePage from './routes/case/CaseId/index.jsx'
 
-function App() {
+const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/case/:id" element={<CasePage />} />
+        {/* Add additional routes below */}
+        {/* Example: <Route path="/" element={<Dashboard />} /> */}
       </Routes>
     </BrowserRouter>
   )
 }
+
+export default App
