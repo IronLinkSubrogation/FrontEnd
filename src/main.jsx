185adec275ffd9d59a2153e29d2fba62bb533bea
IronLinkSubrogation <ironlinksubrogation@gmail.com>
2025-07-24 20:32:55 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import './index.css';
-
 import Dashboard from './routes/dashboard/index.jsx';
 import CaseRoute from './routes/case/[id]/index.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+export default function App() {
+  return (
     <BrowserRouter>
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/case/:id" element={<CaseRoute />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
-);
+  );
+}
